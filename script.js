4e1122b9d78c85525029778b48cd7894660a3d84
guesswhojustme <imnayeonsaranghaee@gmail.com>
2024-09-03 11:23:09 +0800

    Improve the grid div inside the drawing container
    Made the grid div dynamically adjust base on the given amount of squares per side

--- script.js
+++ script.js
@@ -1,15 +1,39 @@
-const container = document.getElementById('container');
- 
-function createDiv(){
-    const div = document.createElement("div");
-    container.appendChild(div)    
+const drawingContainer = document.getElementById('container');
+const button = document.getElementById('btn');
+
+function createDiv(size) {
+    const gridDiv = document.createElement("div");
+    gridDiv.style.width = `${size}px`;
+    gridDiv.style.height = `${size}px`;
+    gridDiv.className = "divman"
+    drawingContainer.appendChild(gridDiv);    
 }
 
-function divs16by16(){
-    for(i = 0; i <= 255; i++){   
-        createDiv();
+function divDefault(){
+    const size = 800 / 16; 
+    for (let i = 0; i < 16 * 16; i++) {   
+        createDiv(size);
     }
 };
 
-divs16by16();
+divDefault();
+
+button.addEventListener('click', () => {
+    let val = prompt("SET NUMBER OF SQUARES PERSIDE (100max)");
+    while (drawingContainer.firstChild) {
+        drawingContainer.removeChild(drawingContainer.firstChild);
+    }
+    if(val < 101){
+        const size = 800 / val;
+        for(i = 0; i < val * val; i++){
+            createDiv(size);
+        }
+    } 
+    else{
+        divDefault();
+        alert("exceeded the limits");
+    }
+});
+
+
 
